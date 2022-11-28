11833f37bf89fd4f9aa10f7a7abbfb107ed894e5
Filip Breska <filixed@gmail.com>
2022-11-28 13:00:01 +0100

    microsoft tye config

--- BasicMvcWithGenericRepository/Program.cs
+++ BasicMvcWithGenericRepository/Program.cs
@@ -16,6 +16,14 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<PostgreDbContext>();    
+    context.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
